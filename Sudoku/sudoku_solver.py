4151485c47bfc53c31623c3e4aa48f26100be4e0
reshcmd3-sys <reshcmd3@gmail.com>
2026-04-24 11:33:22 +0530

    Implement Sudoku solver using backtracking

---
+++ Sudoku/sudoku_solver.py
@@ -0,0 +1,67 @@
+# Sudoku Solver using Backtracking (CSP)
+
+def is_valid(board, row, col, num):
+    # Check row
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check 3x3 grid
+    start_row = row - row % 3
+    start_col = col - col % 3
+
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+
+                        if solve_sudoku(board):
+                            return True
+
+                        board[row][col] = 0  # Backtrack
+                return False
+    return True
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+
+# Sample Sudoku Input
+board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+print("Original Sudoku:")
+print_board(board)
+
+if solve_sudoku(board):
+    print("\nSolved Sudoku:")
+    print_board(board)
+else:
+    print("No solution exists")
